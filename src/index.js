adf269c42a562d2c13ef923a1fee8db6b1ef2aac
Vander-Reis <vanderreis2017@outlook.com>
2022-12-16 14:35:04 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,78 @@
+const express = require("express");
+
+const { v4: uuid } = require("uuid");
+
+const app = express();
+
+app.use(express.json());
+
+const repositories = [];
+
+function checksExistsRepository( request, response, next ) {
+  const { id } = request.params;
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+
+  if (repositoryIndex < 0) {
+    return response.status(404).json({ error: "Repository not found" });
+  }
+
+  request.repositoryIndex = repositoryIndex;
+
+  return next();
+} 
+
+app.get("/repositories", (request, response) => {
+  return response.json(repositories);
+});
+
+app.post("/repositories", (request, response) => {
+  const { title, url, techs } = request.body
+
+  const repository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0
+  };
+
+  repositories.push(repository);
+
+  return response.status(201).json(repository);
+});
+
+app.put("/repositories/:id", checksExistsRepository, (request, response) => {
+
+  const { title, url, techs } = request.body; 
+
+  const { repositoryIndex } = request 
+
+  const repository = { ...repositories[repositoryIndex], title, url, techs };
+
+  repositories[repositoryIndex] = repository;
+
+  return response.json(repository);
+});
+
+app.delete("/repositories/:id", checksExistsRepository, (request, response) => {
+  
+  const { repositoryIndex } = request;
+
+  repositories.splice(repositoryIndex, 1);
+
+  return response.status(204).send();
+});
+
+app.post("/repositories/:id/like", checksExistsRepository, (request, response) => {
+  
+  const { repositoryIndex } = request;
+
+  const repository = repositories[repositoryIndex];
+
+  repository.likes++;
+
+  return response.json(repository);
+});
+
+module.exports = app;
